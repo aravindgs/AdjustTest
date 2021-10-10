626ff6c7f438c48d1da0e6591f9ce1ebcaf3ca9e
aravindgs <aravindgs007@gmail.com>
2021-10-10 17:06:15 +0530

    Button click second extraction and basic checks if second was already sent

--- AdjustTest/AdjustTest/ViewController.m
+++ AdjustTest/AdjustTest/ViewController.m
@@ -9,6 +9,8 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) NSMutableArray<NSNumber *> *sentSeconds;
+
 @end
 
 @implementation ViewController
@@ -16,7 +18,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.sentSeconds = [[NSMutableArray alloc] init];
 }
 
+- (IBAction)captureSecond {
+    if ([self.sentSeconds count] >= 60) { return; }
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"ss"];
+    NSInteger seconds = [[dateFormatter stringFromDate:[NSDate date]] integerValue];
+    if ([self.sentSeconds containsObject:[NSNumber numberWithInteger:seconds]]) { return; }
+    [self.sentSeconds addObject:[NSNumber numberWithInteger:seconds]];
+    [self sendSecondToServer:seconds];
+}
+
+- (void) sendSecondToServer:(NSInteger)second {
+    
+}
 
 @end
