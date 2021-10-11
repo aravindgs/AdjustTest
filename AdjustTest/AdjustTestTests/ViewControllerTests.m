69b779b66331a72bef14ac8a8be7852c323f6469
aravindgs <aravindgs007@gmail.com>
2021-10-11 13:04:07 +0530

    Removed unused test

--- AdjustTest/AdjustTestTests/ViewControllerTests.m
+++ AdjustTest/AdjustTestTests/ViewControllerTests.m
@@ -56,11 +56,4 @@
     XCTAssert([finalUnsent isEqualToArray:@[]]);
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
-
 @end
